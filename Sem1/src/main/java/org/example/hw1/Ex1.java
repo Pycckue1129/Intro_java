7357c7698ec1e1d9e288c57810a6d9c0347f23ae
Artem <burkun_artem@mail.ru>
2023-03-03 13:28:42 +0300

    Hw complete

--- Sem1/src/main/java/org/example/hw1/Ex1.java
+++ Sem1/src/main/java/org/example/hw1/Ex1.java
@@ -1,19 +1,5 @@
 package org.example.hw1;
 
-//public class Ex1 {
-//    public static void main(String[] args) {
-//        String s = " the sky is   blue ".replaceAll("\\s+", " ");
-//        System.out.println(s);
-//        String[] arr = s.split(" ");
-//        for (int i = arr.length - 1; i > 0; i--) {
-//            if (i != 1) {
-//                System.out.print(arr[i] + " ");
-//            }else {
-//                System.out.print(arr[i]);
-//            }
-//        }
-//    }
-//}
 
 public class Ex1 {
     public static void main(String[] args) {
