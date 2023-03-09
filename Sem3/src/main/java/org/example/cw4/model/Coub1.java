7d1d26a4e5a003c592efb17faf9eb953f97615f1
Artem <burkun_artem@mail.ru>
2023-03-09 21:38:13 +0300

    sem 090323 done

---
+++ Sem3/src/main/java/org/example/cw4/model/Coub1.java
@@ -0,0 +1,12 @@
+package org.example.cw4.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Coub1 {
+    private String color;
+    private Integer size;
+    private String material;
+}
