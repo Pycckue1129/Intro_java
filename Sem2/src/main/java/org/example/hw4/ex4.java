c0821e112e92ca02ec85031dc5dc415d22ec4744
Artem <burkun_artem@mail.ru>
2023-03-09 12:57:08 +0300

    Homework EX4 090323 done

--- Sem2/src/main/java/org/example/hw4/ex4.java
+++ Sem2/src/main/java/org/example/hw4/ex4.java
@@ -24,25 +24,45 @@ public class ex4 {
                 {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
         };
-        System.out.println(Integer.parseInt(String.valueOf(board[0][1])) + Integer.parseInt(String.valueOf(board[0][2])));
+        System.out.println("Validate:" + isValidSudoku(board));
+    }
+
+    private static boolean isValidSudoku(char[][] board) {
+        boolean flag = true;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                for (int k = j + 1; k < board.length; k++) {
+                    if (board[i][j] == board[i][k] && board[i][j] != '.') {
+                        flag = false;
+                    }
+                    if (board[j][i] == board[k][i] && board[j][i] != '.') {
+                        flag = false;
+                    }
+                }
+            }
+        }
+        for (int m = 0; m < 9; m += 3) {
+            for (int n = 0; n < 9; n += 3) {
+                for (int i = m; i < m + 3; i++) {
+                    for (int j = n; j < n + 3; j++) {
+                        for (int k = m; k < m + 3; k++) {
+                            for (int l = n; l < n + 3; l++) {
+                                String i2 = String.valueOf(i);
+                                String j2 = String.valueOf(j);
+                                String k2 = String.valueOf(k);
+                                String l2 = String.valueOf(l);
+                                if (!(i2 + j2).equals(k2 + l2)){
+                                    if (board[i][j] == board[k][l] && board[i][j] != '.'){
+                                        return false;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return flag;
     }
 }
-//            System.out.println("Validete:" + isValidSudoku(board));  }
-
-//    private static boolean isValidSudoku(char[][] board) {
-//            boolean flag = true;
-//        for (int i = 0; i < board.length; i++) {
-//            for (int j = 0; j < board.length; j++) {
-//                for (int k = 1; k < board.length; k++) {
-//                    if (Integer.parseInt(String.valueOf(board[i][j])) == Integer.parseInt(String.valueOf(board[i][k]))){
-//                        flag = false;
-//                    }
-//                    if (board[j][i] == board[k][i]){
-//                        flag = false;
-//                    }
-//                }
-//            }
-//        }
-//        return flag;
-//    }
-//}
